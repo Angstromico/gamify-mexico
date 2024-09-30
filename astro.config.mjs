403e9858c6bace831630fee699668d105dc45644
Manuel Esteban Morales Zuarez <manuesteban1990@gmail.com>
2024-09-29 22:22:55 -0400

    No Vercel Server

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,11 +5,7 @@ import tailwind from '@astrojs/tailwind'
 
 import react from '@astrojs/react'
 
-import vercel from '@astrojs/vercel/serverless'
-
 // https://astro.build/config
 export default defineConfig({
-  output: 'server',
   integrations: [tailwind(), react()],
-  adapter: vercel(),
 })
