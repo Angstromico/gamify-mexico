c1837253b45044afc6245793e2a1c6902e42b6e4
Manuel Esteban Morales Zuarez <manuesteban1990@gmail.com>
2024-10-07 17:44:56 -0400

    Test fix langContainer href

--- src/components/LangContainer.tsx
+++ src/components/LangContainer.tsx
@@ -11,18 +11,24 @@ const LangContainer = ({ languages, path }: Props) => {
   const currentLang = path.includes('/en') ? 'en' : 'es'
 
   const getHref = (lang: string) => {
-    if (
-      (lang === 'en' && currentLang === 'en') ||
-      (lang === 'es' && currentLang === 'es')
-    ) {
-      return path
-    }
-    if (lang === 'es' && currentLang === 'en') {
-      const filterPath = path.replace('/en', '') // Remove '/en' prefix
-      return filterPath
+    // If language is English and the current path is already in English
+    if (lang === 'en' && currentLang === 'en') {
+      return path // Return the same path, no change needed
     }
 
-    return 'en' + path
+    // If switching to Spanish from English, remove '/en' from path
+    if (lang === 'es' && currentLang === 'en') {
+      const filterPath = path.replace('/en', '') // Remove '/en' prefix
+      return filterPath || '/' // Fallback to '/' if path becomes empty
+    }
+
+    // If switching to English and the path doesn't already have '/en'
+    if (lang === 'en' && currentLang === 'es') {
+      return '/en' + path // Add '/en' to the existing path
+    }
+
+    // Default return for other cases
+    return path
   }
   return (
     <>
