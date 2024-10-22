41b8fb6a331ab866f8f7e4d619fc31f7ce55704e
Manuel Esteban Morales Zuarez <manuesteban1990@gmail.com>
2024-10-21 23:37:17 -0400

    Vercel serverside

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,8 +5,11 @@ import tailwind from '@astrojs/tailwind'
 
 import react from '@astrojs/react'
 
+import vercel from '@astrojs/vercel/serverless'
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), react()],
-  output: 'server', // Or "hybrid"
+  output: 'server', // Or "hybrid",
+  adapter: vercel(),
 })
