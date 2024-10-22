7e8f1f82c2783aad84a513c1229dd6edd37a20f6
Manuel Esteban Morales Zuarez <manuesteban1990@gmail.com>
2024-10-21 23:50:03 -0400

    Netlify serverside

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,14 +2,12 @@
 import { defineConfig } from 'astro/config'
 
 import tailwind from '@astrojs/tailwind'
-
 import react from '@astrojs/react'
-
-import vercel from '@astrojs/vercel/serverless'
+import netlify from '@astrojs/netlify' // Use Netlify adapter
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), react()],
-  output: 'server', // Or "hybrid",
-  adapter: vercel(),
+  output: 'server', // Keep it as 'server' or 'hybrid' for serverless functions
+  adapter: netlify(), // Use Netlify adapter
 })
